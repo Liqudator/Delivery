be9c08fe4e5c60c480fa3b065084470cd608aac0
Денисов Сергей (Denisov_SV) <denisov_sv@directum.ru>
2019-06-10 09:32:51 +0400

    Добавил доменные модели

---
+++ Delivery.WebService/Models/Domain/Seller.cs
@@ -0,0 +1,38 @@
+﻿namespace Delivery.WebService.Models.Domain
+{
+    /// <summary>
+    /// Модель продавца.
+    /// </summary>
+    public class Seller
+    {
+        /// <summary>
+        /// ИД.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Фамилия.
+        /// </summary>
+        public string SecondName { get; set; }
+
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Отчество.
+        /// </summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>
+        /// Город проживания.
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Размер комиссии в процентах.
+        /// </summary>
+        public double Comission { get; set; }
+    }
+}
---
+++ Delivery/Domain/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Delivery.Domain
+{
+    /// <summary>
+    /// Модель заказа.
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// ИД заказа.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Описание заказа.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Сумма заказа.
+        /// </summary>
+        public double Sum { get; set; }
+
+        /// <summary>
+        /// Дата-время заказа.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// ИД покупателя.
+        /// </summary>
+        public int BuyerId { get; set; }
+
+        /// <summary>
+        /// ИД продавца.
+        /// </summary>
+        public int SellerId { get; set; }
+    }
+}
